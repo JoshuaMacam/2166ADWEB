b231bc1e00df35de8de469f4835502f8021515fc
JoshuaMacam <122767207+JoshuaMacam@users.noreply.github.com>
2024-02-01 23:54:06 +0800

    main.component.ts

---
+++ Prelim Lab Exam/main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent {
+  name = 'Joshua Emmanuel Macam'
+  course = 'Coffee Shop Owner☕'
+  description = "I am Joshua from Mabalacat Pampanga. I am currently working as a owner of a coffee shop called JUA. The shop was founded by me on December 24, 2023. Aubrey Lisber Castro is actually the co-owner of this coffee shop. "
+}
